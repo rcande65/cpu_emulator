030925d7b7e066b79e4885482bf13dedc088553c
rcande65 <115851308+rcande65@users.noreply.github.com>
2025-08-30 21:52:36 -0700

    Fixed path of program file with new Programs directory

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ DEBUG = 0
 my_cpu = cpu("My CPU", inst_mem_size=2048, data_mem_size=2048)
 my_cpu.reset(debug=DEBUG)
 
-program_file = "program_4.txt"
+program_file = "./Programs/program_4.txt"
 
 compile_program(my_cpu, program_file)
 my_cpu.run_program(print_instruction=DEBUG),
